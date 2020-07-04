a936e5d38efe61162766b2ff072559c6f390aecc
BuckwheatTTS <66149120+BuckwheatTTS@users.noreply.github.com>
2020-07-04 10:49:50 -0600

    Create _contents.lua

---
+++ ui_assets/side_menu/_contents.lua
@@ -0,0 +1,8 @@
+local genTable = require("The-Adventurers-Guild/ui_assets/_gen")
+local path = "side_menu"
+local contents = {
+  "side_menu_solid",
+  "side_menu_faded",
+}
+
+return genTable(path, contents)
